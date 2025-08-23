46dcb6421dc1f1d6d057d83e9a8f6d73268e2753
amina041 <happykahn3002@gmail.com>
2025-08-23 23:18:15 +0600

    code 6 7 8

---
+++ basic/6_.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+    float a, b,ans;
+
+    printf("Enter two floating point numbers: ");
+    scanf("%f %f",&a, &b);
+
+    ans=a/b;
+
+    printf("ans is %f",ans);
+
+    return 0;
+
+}
