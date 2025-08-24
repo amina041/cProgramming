c587639e10e462ef79b0fbcdd7f710ab256a8af8
amina041 <happykahn3002@gmail.com>
2025-08-24 23:44:37 +0600

    10

--- basic/10_.c
+++ basic/10_.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+int main() {
+    int m,n,o,p;
 
+    m = sizeof(int);
+    n = sizeof(char);
+    o = sizeof(float);
+    p = sizeof(double);
+
+    printf("size of integer is: %d\n",m);
+    printf("size of character is: %d\n",n);
+    printf("size of float is: %d\n",o);
+    printf("size of double is: %d\n",p);
+
+    return 0;
+}
