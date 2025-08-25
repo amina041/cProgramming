d384a36c3213bc72eba18146f2a3bb1cbacada97
amina041 <happykhan3002@gmail.com>
2025-08-25 22:02:53 +0600

    13

--- basic/13_simpleInterst.c
+++ basic/13_simpleInterst.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(){
-float P,T,R,SI;
+float P,T,R;
 printf("Enter P,T,R: ");
 scanf("%f %f %f",&P,&T,&R);
-SI = P*T*R/100;
+float SI = P*T*R/100;
 printf("Simple interest is: %f",SI);
 return 0;
 }
---
+++ looptest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d", &n);
+    for(int a = 1 ; a<=n; a++){
+
+         printf("%d \n",a);
+
+    }
+
+    return 0;
+}
