09276bdc715f9e8bc187fdb382d0ee11a041bb46
amina041 <happykhan3002@gmail.com>
2025-08-28 21:43:00 +0600

    15, 16

---
+++ basic/15_.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a,b,P;
+
+    printf("Enter perimeter of rectangle: ");
+    scanf("%d %d",&a ,&b);
+
+    P = (2*a + 2*b);
+
+    printf("perimeter is %d",P);
+
+return 0;
+}
