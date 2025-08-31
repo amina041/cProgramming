12ca60966bb8fd3bb6f79c3cf63ef8b7e73e2680
amina041 <happykhan3002@gmail.com>
2025-08-31 20:35:03 +0600

    17 - 22

---
+++ control flow/19_.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main (){
+int a,b,c;
+printf("Enter three number: ");
+scanf("%d %d %d",&a,&b,&c);
+if(a>=b && a>=c){
+printf("%d largest number: ",a);
+}
+else if(b>=c && b>=a){
+printf("%d largest number: ",b);
+}
+else{
+printf("%d largest number: ",c);
+
+}
+    return 0;
+}
+
+
