a6e32f8b28240c30c56bf0c3590690db31c4c843
amina041 <happykhan3002@gmail.com>
2025-09-01 21:28:53 +0600

    24

---
+++ control flow/24_.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+double a,b;
+char c;
+
+    printf("Enter value: ");
+    scanf("%d %c %d",&a,&c,&b);
+
+    if(c == '+'){
+        printf("= %f",a+b);
+    }
+
+    if(c == '-'){
+        printf("= %f",a-b);
+    }
+
+    if(c == '*'){
+        printf("= %f",a*b);
+    }
+
+    if(c == '/'){
+        printf("= %f",a/b);
+    }
+
+return 0;
+}
+
